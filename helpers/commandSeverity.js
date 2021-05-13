008244373513c03215fd43bb32460fd7e8b52e5c
Facundo Gauna <fgauna12@gmail.com>
2021-05-13 16:20:58 -0400

    Revert "Revert "Wip - Adding results""
    This reverts commit fcaea6197f68b92c97474cb5487c358f5cd0e545.

---
+++ helpers/commandSeverity.js
@@ -0,0 +1,9 @@
+
+export const Severity = Object.freeze(
+    { 
+        "High": "High", 
+        "Medium": "Medium", 
+        "Low": "Low",
+        "Informational": "Informational"
+    }
+)
---
+++ helpers/commandStatus.js
@@ -0,0 +1,8 @@
+
+export const ResultStatus = Object.freeze(
+    { 
+        "Pass": true, 
+        "Fail": false, 
+        "NotApply": undefined 
+    }
+)
