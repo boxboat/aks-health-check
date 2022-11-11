a443faf181fa071cd339ef5b4344ddfe626c471d
Facundo Gauna <fgauna12@gmail.com>
2022-11-11 12:13:24 -0800

    Adding simple terraform configuration
    Signed-off-by: Facundo Gauna <fgauna12@gmail.com>

---
+++ tests/scenario/simple-cluster/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-simple-cluster"
+  location = "eastus"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-simple-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks-simple-cluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B1s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
